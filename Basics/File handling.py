8f2f2816249bb95514685e6444e4aef40c8d4ed7
Jyoti Gupta <160124343+Jo-shreer@users.noreply.github.com>
2025-07-10 14:46:17 +0100

    Create File handling.py

---
+++ Basics/File handling.py
@@ -0,0 +1,11 @@
+# Write to a file
+with open("example.txt", "w") as f:
+    f.write("Hello, file!")
+
+
+
+# Read from a file
+with open("example.txt") as f:
+    content = f.read()
+    print(content)
+  
