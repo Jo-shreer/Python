6a65370e8f41b9d834186b256e35df87df9c4902
Jyoti Gupta <160124343+Jo-shreer@users.noreply.github.com>
2026-09-01 16:01:06 +0100

    Update 04. Function.py

--- Basics/Basics/04. Function.py
+++ Basics/Basics/04. Function.py
@@ -3,28 +3,21 @@ def greet(name):
 
 print(greet("Alice"))
 
-
 op
 Hello, Alice!
 
-
-
 #function eith default argument 
-
 def greet(name="stranger"):
     return f"Hello, {name}!"
 
 print(greet())
 print(greet("Bob"))
 
-
 op
 Hello, stranger!
 Hello, Bob!
 
-
 #with multiple arguments
-
 def add(a, b):
     return a + b
 
